63f28ef3668fdc2768f0fb08d12fab6a809d351b
Miguel S. da Silva <miguelss616@outlook.com.br>
2025-10-19 11:29:09 -0300

    Initial commit

---
+++ src/main/java/br/com/scripta/scripta_api/ScriptaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.scripta.scripta_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/scripta/scripta_api/ScriptaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.scripta.scripta_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
